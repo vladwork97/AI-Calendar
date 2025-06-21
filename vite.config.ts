ae400648a914b40af1fcc705edb22feb493d5b17
vladwork97 <vladwork97@gmail.com>
2025-06-21 17:08:27 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ai-calendar-ui/',
+  base: '/AI-Calendar/',
   plugins: [react(), tailwindcss()],
 })
